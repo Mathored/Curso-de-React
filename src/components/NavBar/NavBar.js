047e15da77492d6cc49ac56082e9af2caaf94c84
Matias Alvarez <leandromatiasalvarez@gmail.com>
2022-01-12 07:08:21 -0300

    Actualización clase 3

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export const NavBar = () => {
+    return (
+        <div className='Nav-bar'>
+            <ul className='Ul-nav'>
+                <li>Categorías</li>
+                <li>Ofertas</li>
+                <li>Mis favoritos</li>
+                <li>Mis compras</li>
+            </ul>
+        </div>
+    )
+}
